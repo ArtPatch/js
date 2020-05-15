7dc1a99a23e8f9f9bc18a7ab17a2ed86a88aca1f
Artem <artemivlev@yandex.ru>
2020-05-15 17:54:02 +0300

    New lesson
    Switch construction

--- project_1/script.js
+++ project_1/script.js
@@ -1,20 +1,16 @@
-let money = prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD');
+const num = 50;
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses:[],
-    income: [],
-    savings: false
-};
-let a1 = prompt('Введите обязательную статью расходов в этом меясце', ''),
-    a2 = prompt('Во сколько обойдется?', ''),
-    a3 = prompt('Введите обязательную статью расходов в этом меясце', ''),
-    a4 = prompt('Во сколько обойдется?', '');
-
-appData.expenses[a1]=[a3];
-appData.expenses[a2]=[a4];
-
-alert(appData.budget / 30);
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Совершенно верно!');
+        break;
+      default:
+          console.log('Не в этот раз!');
+          break;
+}
